858f9e0c6e995d0172f93110f57eca02ea94b11c
Parth Panchal <parth.panchal1729@gmail.com>
2026-09-11 21:56:55 +0530

    feat:decode added

--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,22 @@
 class Tokenizer:
     def __init__(self):
-        with open("training_data.txt", "r", encoding="utf-8") as file:
+        with open("input.txt", "r", encoding="utf-8") as file:
                 self.train_text = file.read()
         self.merge_map = {}
-        self.merge_limit = 50
+        self.merge_limit = 500
         self.next_id = 256
         #self.ids = list(text.encode('utf-8'))
 
+    def build_vocab(self):
+        vocab = {idx: bytes([idx]) for idx in range(256)}   # base case: single bytes
+        for pair, new_id in self.merge_map.items():
+            vocab[new_id] = vocab[pair[0]] + vocab[pair[1]]  # concatenate bytes of the merged pair
+        self.vocab = vocab
+    
+    def decode(self, ids):
+        bytes_out = b"".join(self.vocab[idx] for idx in ids)
+        return bytes_out.decode("utf-8", errors="replace")
+    
     def pair_counts(self,ids):
         pair_counts = {}
         for i in range(len(ids) - 1):
@@ -26,9 +36,9 @@ class Tokenizer:
                 i += 1
         return new_ids
 
-    def train(self, merge_limit):
-         ids = list(self.train_text.encode('utf-8'))
-         for i in range(merge_limit):
+    def train(self):
+        ids = list(self.train_text.encode('utf-8'))
+        for i in range(self.merge_limit):
             counts = self.pair_counts(ids)
             if not counts:
                 break
@@ -36,19 +46,20 @@ class Tokenizer:
             ids = self.merge_pair(ids, max_pair, self.next_id)
             self.merge_map[max_pair] = self.next_id
             self.next_id += 1
+        self.build_vocab()
 
     def encode(self, text):
         ids = list(text.encode("utf-8"))
-
         while len(ids) >= 2:
             counts = self.pair_counts(ids)
             pair_to_merge = min(counts, key=lambda p: self.merge_map.get(p, float("inf")))
-
             if pair_to_merge not in self.merge_map:
                 break
-
             ids = self.merge_pair(ids, pair_to_merge, self.merge_map[pair_to_merge])
-
         return ids
 
+obj = Tokenizer()
+obj.train()
+x = obj.encode("thou")
+print(x)
 
